05a40a560838c374dd60fdcba4c94a3c24089028
Antonio214 <antoniojuniorlk366@gmail.com>
2023-11-02 21:33:31 -0300

    implementação do servico de eventos usando firebas

---
+++ src/api/firebaseImp/eventos.service.ts
@@ -0,0 +1,48 @@
+// eventService.ts
+import { db } from '../../config/firebase';
+import { doc, getDoc, addDoc, collection } from 'firebase/firestore';
+import {
+  PayloadNovoEvento,
+  RespostaNovoEvento,
+  DadosEvento,
+  IEventoApi,
+} from '../eventos.api';
+
+class EventoService implements IEventoApi {
+  private readonly collectionName = 'Eventos';
+
+  // Method to create a new event
+  async criarNovoEvento(
+    payload: PayloadNovoEvento
+  ): Promise<RespostaNovoEvento> {
+    try {
+      // Add a new document with a generated id in the "eventos" collection
+      const docRef = await addDoc(collection(db, this.collectionName), payload);
+      return {
+        sucesso: true,
+        codigoEvento: docRef.id,
+      };
+    } catch (error) {
+      // Handle any errors here, such as logging or throwing an application-specific error
+      console.error('Error adding document: ', error);
+      throw new Error('Failed to create a new event');
+    }
+  }
+
+  // Method to fetch an event by its code
+  async buscarEventoPorCodigo(codigo: string): Promise<DadosEvento> {
+    const docRef = doc(db, this.collectionName, codigo);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      // Cast the data to DadosEvento type
+      return docSnap.data() as DadosEvento;
+    } else {
+      // Handle the case where the document does not exist
+      throw new Error(`No event found with code: ${codigo}`);
+    }
+  }
+}
+
+// Export an instance of the service
+export const eventoService = new EventoService();
