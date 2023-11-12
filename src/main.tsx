6105c9b020722ba06d8fb9ff90338bd6eb65dcdd
EijiTomonari <gabriel@tomonari.dev>
2023-11-12 20:03:48 -0300

    Adiciona router e rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import React from 'react'
-import { extendTheme } from '@chakra-ui/react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import { extendTheme } from '@chakra-ui/react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { browserRouter } from './routes/routes.tsx';
 
 const colors = {
   brand: {
@@ -11,15 +13,15 @@ const colors = {
     800: '#153e75',
     700: '#2a69ac',
   },
-}
+};
 
-const theme = extendTheme({ colors })
-
+const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <RouterProvider router={browserRouter} />
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
