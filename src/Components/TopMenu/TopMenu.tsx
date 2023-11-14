8ad4bccc332137da1b34f22d3e8f2f6d771752a1
Gabriel Tadeu <gabriel-tadeu@proton.me>
2023-11-14 17:34:31 -0300

    formatando algumas paginas da maneira correta

--- src/Components/TopMenu/TopMenu.tsx
+++ src/Components/TopMenu/TopMenu.tsx
@@ -1,23 +1,35 @@
 // TopMenu.tsx
 import React from 'react';
-import {Flex, StyleProps} from '@chakra-ui/react';
+import { Flex, StyleProps } from '@chakra-ui/react';
 import AppLogo, { LogoSize } from '../AppLogo/AppLogo';
 import SimpleText from '../SimpleText/SimpleText';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 export interface TopMenuProps extends StyleProps {}
 
-
-
-const TopMenu: React.FC<TopMenuProps> = ({...styleProps}) => {
-    const navigate = useNavigate();
-    return <Flex flexDir='line' textAlign='center' {...styleProps}>
-        <Flex ml="0.6vw" mr="1vw" onClick= {() => navigate("/")}>
-            <AppLogo size={LogoSize.Small} ></AppLogo>
-        </Flex>
-        <Flex mr="1vw"><SimpleText value={"Página Inicial"} fontWeight='bold' fontSize='1vw' onClick= {() => navigate("/paginal-inicial")}></SimpleText></Flex>
-        <Flex mr="1vw"><SimpleText value={"Login"} fontWeight='bold' fontSize='1vw' onClick= {() => navigate("/cadastro")}></SimpleText></Flex>
+const TopMenu: React.FC<TopMenuProps> = ({ ...styleProps }) => {
+  const navigate = useNavigate();
+  return (
+    <Flex flexDir="line" textAlign="center" {...styleProps}>
+      <Flex ml="0.6vw" mr="1vw" onClick={() => navigate('/')}>
+        <AppLogo size={LogoSize.Small}></AppLogo>
+      </Flex>
+      <Flex mr="1vw">
+        <SimpleText
+          value={'Página Inicial'}
+          fontWeight="bold"
+          fontSize="1vw"
+          onClick={() => navigate('/paginal-inicial')}></SimpleText>
+      </Flex>
+      <Flex mr="1vw">
+        <SimpleText
+          value={'Login'}
+          fontWeight="bold"
+          fontSize="1vw"
+          onClick={() => navigate('/cadastro')}></SimpleText>
+      </Flex>
     </Flex>
+  );
 };
 
 export default TopMenu;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,50 +8,48 @@ import FinalButton, {
 } from '../Components/FinalButton/FinalButton';
 import TopMenu from '../Components/TopMenu/TopMenu';
 
-const LandingPage = () => {
-  return (
-    <RightImageLayoutComponent imageUrl={'src/assets/pens.png'}>
-      <TopMenu position="absolute" top="2vh"></TopMenu>
-      <Flex flexDir={'column'} verticalAlign={'middle'}>
-        <Flex flexDir={'column'} w={'60%'} alignSelf={'center'}>
-          <AppLogo size={LogoSize.Large}></AppLogo>
-          <TitleText value={'Conectando'} mt={10}></TitleText>
-          <TitleText value="voluntários e causas"></TitleText>
-          <TitleText value="para transformar vidas."></TitleText>
-          <Flex flexDir={'column'} alignSelf={'center'} mt={6}>
-            <SimpleText
-              value={
-                'Seja parte de uma comunidade que faz a diferença! Com o VoluntariApp, você pode criar um evento voluntário ou participar de um evento já existente '
-              }></SimpleText>
-          </Flex>
-        </Flex>
-        <Flex flexDir={'column'} width={'100%'} textAlign={'center'}>
-          <FinalButton
-            label={'criar evento'}
-            style={{
-              type: ButtonStyleOptions.Primary,
-              width: '20vw',
-              mt: 14,
-              mb: 4,
-            }}
-            onClick={function (): void {
-              throw new Error('Function not implemented.');
-            }}></FinalButton>
-          <FinalButton
-            label={'entrar com código de evento'}
-            style={{
-              type: ButtonStyleOptions.Secondary,
-              width: '20vw',
-              mt: 4,
-              mb: 4,
-            }}
-            onClick={function (): void {
-              throw new Error('Function not implemented.');
-            }}></FinalButton>
+const LandingPage = () => (
+  <RightImageLayoutComponent imageUrl={'src/assets/pens.png'}>
+    <TopMenu position="absolute" top="2vh"></TopMenu>
+    <Flex flexDir={'column'} verticalAlign={'middle'}>
+      <Flex flexDir={'column'} w={'60%'} alignSelf={'center'}>
+        <AppLogo size={LogoSize.Large}></AppLogo>
+        <TitleText value={'Conectando'} mt={10}></TitleText>
+        <TitleText value="voluntários e causas"></TitleText>
+        <TitleText value="para transformar vidas."></TitleText>
+        <Flex flexDir={'column'} alignSelf={'center'} mt={6}>
+          <SimpleText
+            value={
+              'Seja parte de uma comunidade que faz a diferença! Com o VoluntariApp, você pode criar um evento voluntário ou participar de um evento já existente '
+            }></SimpleText>
         </Flex>
       </Flex>
-    </RightImageLayoutComponent>
-  );
-};
+      <Flex flexDir={'column'} width={'100%'} textAlign={'center'}>
+        <FinalButton
+          label={'criar evento'}
+          style={{
+            type: ButtonStyleOptions.Primary,
+            width: '20vw',
+            mt: 14,
+            mb: 4,
+          }}
+          onClick={function (): void {
+            throw new Error('Function not implemented.');
+          }}></FinalButton>
+        <FinalButton
+          label={'entrar com código de evento'}
+          style={{
+            type: ButtonStyleOptions.Secondary,
+            width: '20vw',
+            mt: 4,
+            mb: 4,
+          }}
+          onClick={function (): void {
+            throw new Error('Function not implemented.');
+          }}></FinalButton>
+      </Flex>
+    </Flex>
+  </RightImageLayoutComponent>
+);
 
 export default LandingPage;
