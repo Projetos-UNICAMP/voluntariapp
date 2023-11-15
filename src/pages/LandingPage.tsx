34cf7df64dc1f570c5989781e595c35d13d817b0
Gabriel Tadeu <gabriel-tadeu@proton.me>
2023-11-15 00:55:24 -0300

    comecando a construcao do pop up da landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,15 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button
+} from '@chakra-ui/react';
 import RightImageLayoutComponent from '../Layouts/RigthImageLayout/RigthImageLayout';
 import AppLogo, { LogoSize } from '../Components/AppLogo/AppLogo';
 import TitleText from '../Components/TitleText/TitleText';
@@ -10,6 +21,7 @@ import TopMenu from '../Components/TopMenu/TopMenu';
 import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+  const {isOpen, onOpen, onClose} = useDisclosure();
   const navigate = useNavigate();
   return (
     <RightImageLayoutComponent imageUrl={'src/assets/pens.png'}>
@@ -36,9 +48,7 @@ const LandingPage = () => {
               mt: 14,
               mb: 4,
             }}
-            onClick={function (): void {
-              throw new Error('Function not implemented.');
-            }}></FinalButton>
+            onClick={onOpen}></FinalButton>
           <FinalButton
             label={'entrar com código de evento'}
             style={{
@@ -63,6 +73,23 @@ const LandingPage = () => {
             }}></FinalButton>
         </Flex>
       </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            asdasdadasdadasdadas
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </RightImageLayoutComponent>
   );
 };
