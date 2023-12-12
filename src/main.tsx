7fb25ab12a52e7b2739c9500fd034ea9b3e17844
EijiTomonari <gabriel@tomonari.dev>
2023-12-11 23:51:50 -0300

    Corrige erros do linter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Router, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import { browserRouter } from './routes/routes.tsx';
 import { AuthProvider } from './Providers/AuthProvider.tsx';
